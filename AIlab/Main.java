87e4b70bc620ae47542ac1e058c360c503fed4dc
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2022-11-16 18:48:46 +0300

    Update Main.java

--- AIlab/Main.java
+++ AIlab/Main.java
@@ -27,7 +27,7 @@ public class Main {
 
         System.out.println("\n"+"=".repeat(100)+"\n");
 
-        /*Запуск неинформированных алгоритмов поиска*/
+        /*Запуск информированных алгоритмов поиска*/
         City start = citiesDataParser.getCityFromMap("Самара");
         City finish = citiesDataParser.getCityFromMap("Ярославль");
 
